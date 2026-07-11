87bb2b38f3a5c8dab6d71026025080161f096240
Christopher Waschke <chrisdwaschke@gmail.com>
2026-07-11 16:18:28 -0700

    Initial Commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Auditor.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}

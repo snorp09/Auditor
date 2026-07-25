3216ee9ccb52749c9f8ac305fa70328e49f7d665
Christopher Waschke <chrisdwaschke@gmail.com>
2026-07-25 14:52:44 -0700

    Add transaction type property.

--- Migrations/20260725211411_AddTransactionType.Designer.cs
+++ Migrations/20260725211411_AddTransactionType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Auditor.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Auditor.Migrations
 {
     [DbContext(typeof(AuditorDb))]
-    partial class AuditorDbModelSnapshot : ModelSnapshot
+    [Migration("20260725211411_AddTransactionType")]
+    partial class AddTransactionType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.10");

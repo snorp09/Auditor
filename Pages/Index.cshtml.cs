70f99bc31872d865273e535ca7ad08701fc7e902
Christopher Waschke <Chrisdwaschke@gmail.com>
2026-07-27 22:02:16 -0700

    Redirect root to dashboard.
    Preparing for Official Milestone 1 completion.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,8 +5,8 @@ namespace Auditor.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
+    public IActionResult OnGet()
     {
-
+        return RedirectToPage("/dashboard/index");
     }
 }

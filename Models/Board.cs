620c00f2fb38acc89a753a46bfb94d82268aef85
Christopher Waschke <chrisdwaschke@gmail.com>
2026-08-18 16:23:07 -0700

    Board Name shouldn't be nullable.

--- Models/Board.cs
+++ Models/Board.cs
@@ -3,7 +3,7 @@ namespace Auditor.Models;
 public class Board
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
     public List<Transaction> Transactions { get; set; } = [];
 
